6694cbb92039954df65041425ee298fd55e89d50
Seth Willits <seth@freaksw.com>
2017-03-08 12:06:43 -0800

    Initial Commit

---
+++ BinaryStream/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BinaryStream
+//
+//  Created by Seth Willits on 3/8/17.
+//  Copyright © 2017 Araelium Group. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
