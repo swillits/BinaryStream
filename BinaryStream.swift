3af49700fcfed764e2b083ca19939f5d1b510333
Seth Willits <seth@freaksw.com>
2017-03-08 13:03:20 -0800

    Initial version

---
+++ BinaryStream.swift
@@ -0,0 +1,519 @@
+//
+//  BinaryStream.swift
+//  BinaryStream
+//
+//  Created by Seth Willits on 3/11/16.
+//  Copyright © 2016-2017 Araelium Group. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class BinaryStream {
+	
+	private var _destination: BinaryStreamDestination
+	fileprivate var destination: BinaryStreamDestination {
+		return _destination
+	}
+	
+	init(destination: BinaryStreamDestination) {
+		self._destination = destination
+	}
+	
+	
+	
+	
+	
+	// MARK: - Properties
+	
+	fileprivate let isHostLittleEndian = (UInt32(littleEndian: 1) == 1)
+	
+	/// Determines whether the numeric types are written and read in big endian or little form. This can be changed at any time.
+	var littleEndian: Bool = true
+	
+	
+	/// Returns whether the current position is at the end of the stream
+	var isEndOfStream: Bool {
+		return destination.isEndOfStream
+	}
+	
+	
+	/// The length of the data in the stream.
+	var length: Int {
+		get {
+			return destination.length
+		}
+	}
+	
+	
+	/// The current position of the data in the stream. Reading and writing moves the position.
+	var position: Int {
+		get {
+			return destination.position
+		}
+	}
+	
+	
+	/// Depending on the stream type, setting the position past the length of the stream may:
+	/// a) fail and throw (eg, when using file destination type)
+	/// b) grow the length of the stream (eg, when using data destination type) 
+	func setPosition(_ pos: Int) throws {
+		try destination.setPosition(pos)
+	}
+	
+	
+	/// A simple convenience method for setPosition(position + offset)
+	func offsetPosition(_ offset: Int) throws {
+		try setPosition(position + offset)
+	}
+	
+	
+	// MARK: - Reading
+	
+	func readBytes(_ bytes: UnsafeMutableRawPointer, length: Int) throws {
+		try destination.read(bytes: bytes, length: length)
+	}
+	
+	
+	func readData(length: Int) throws -> Data {
+		return try destination.readData(length: length)
+	}
+	
+	
+	func readBool() throws -> Bool {
+		let b = try readUInt8()
+		return b != 0
+	}
+	
+	
+	func readFloat() throws -> Float {
+		var x: UInt32 = 0
+		try destination.read(bytes: &x, length: 4)
+		x = (littleEndian == isHostLittleEndian) ? x : x.byteSwapped
+		return unsafeBitCast(x, to: Float.self)
+	}
+	
+	
+	func readDouble() throws -> Double {
+		var x: UInt64 = 0
+		try destination.read(bytes: &x, length: 8)
+		x = (littleEndian == isHostLittleEndian) ? x : x.byteSwapped
+		return unsafeBitCast(x, to: Double.self)
+	}
+	
+	
+	func readFloat32() throws -> Float32 {
+		return Float32(try readFloat())
+	}
+	
+	
+	func readFloat64() throws -> Float64 {
+		return Float64(try readDouble())
+	}
+	
+	
+	func readUInt8() throws -> UInt8 {
+		var x: UInt8 = 0
+		try destination.read(bytes: &x, length: 1)
+		return x
+	}
+	
+	
+	func readUInt16() throws -> UInt16 {
+		var x: UInt16 = 0
+		try destination.read(bytes: &x, length: 2)
+		return (littleEndian == isHostLittleEndian) ? x : x.byteSwapped 
+	}
+	
+	
+	func readUInt24() throws -> UInt32 {
+		var x: UInt32 = 0
+		try destination.read(bytes: &x, length: 3)
+		
+		if littleEndian == isHostLittleEndian {
+			return x
+		}
+		
+		return x.byteSwapped >> 8
+	}
+	
+	
+	func readUInt32() throws -> UInt32 {
+		var x: UInt32 = 0
+		try destination.read(bytes: &x, length: 4)
+		return (littleEndian == isHostLittleEndian) ? x : x.byteSwapped
+	}
+	
+	
+	func readUInt64() throws -> UInt64 {
+		var x: UInt64 = 0
+		try destination.read(bytes: &x, length: 8)
+		return (littleEndian == isHostLittleEndian) ? x : x.byteSwapped
+	}
+	
+	
+	func readInt8() throws -> Int8 {
+		return unsafeBitCast(try readUInt8(), to: Int8.self)
+	}
+	
+	
+	func readInt16() throws -> Int16 {
+		return unsafeBitCast(try readUInt16(), to: Int16.self)
+	}
+	
+	
+	func readInt32() throws -> Int32 {
+		return unsafeBitCast(try readUInt32(), to: Int32.self)
+	}
+	
+	
+	func readInt64() throws -> Int64 {
+		return unsafeBitCast(try readUInt64(), to: Int64.self)
+	}
+}
+
+
+
+
+
+class MutableBinaryStream: BinaryStream {
+	
+	fileprivate var mutableDestination: MutableBinaryStreamDestination {
+		return destination as! MutableBinaryStreamDestination
+	}
+	
+	init(destination: MutableBinaryStreamDestination) {
+		super.init(destination: destination)
+	}
+	
+	
+	func write(data: Data) throws {
+		try mutableDestination.write(data)
+	}
+	
+	
+	func write(bytes: UnsafeRawPointer, length: Int) throws {
+		try mutableDestination.write(bytes: bytes, length: length)
+	}
+	
+	
+	func write(float value: Float) throws {
+		try write(float32: Float32(value))
+	}
+	
+	
+	func write(double value: Double) throws {
+		try write(float64: Float64(value))
+	}
+	
+	
+	func write(float32 value: Float32) throws {
+		var x = unsafeBitCast(value, to: UInt32.self)
+		x = littleEndian ? x.littleEndian : x.bigEndian
+		try write(bytes: &x, length: 4)
+	}
+	
+	
+	func write(float64 value: Float64) throws {
+		var x = unsafeBitCast(value, to: UInt64.self)
+		x = littleEndian ? x.littleEndian : x.bigEndian
+		try write(bytes: &x, length: 8)
+	}
+	
+	
+	func write(bool value: Bool) throws {
+		var x: Int8 = value ? 1 : 0
+		try write(bytes: &x, length: 1)
+	}
+	
+	
+	func write(uint8 value: UInt8) throws {
+		var x = value
+		try write(bytes: &x, length: 1)
+	}
+	
+	
+	func write(uint16 value: UInt16) throws {
+		var x = littleEndian ? value.littleEndian : value.bigEndian
+		try write(bytes: &x, length: 2)
+	}
+	
+	
+	func write(uint24 value: UInt32) throws {
+		var x = value
+		
+		if littleEndian == isHostLittleEndian {
+			try write(bytes: &x, length: 3)
+		} else {
+			x = value.byteSwapped
+			x = x >> 8
+			try write(bytes: &x, length: 3)
+		}
+	}
+	
+	
+	func write(uint32 value: UInt32) throws {
+		var x = littleEndian ? value.littleEndian : value.bigEndian
+		try write(bytes: &x, length: 4)
+	}
+	
+	
+	func write(uint64 value: UInt64) throws {
+		var x = littleEndian ? value.littleEndian : value.bigEndian
+		try write(bytes: &x, length: 8)
+	}
+	
+	
+	func write(int8 value: Int8) throws {
+		try write(uint8: unsafeBitCast(value, to: UInt8.self))
+	}
+	
+	
+	func write(int16 value: Int16) throws {
+		try write(uint16: unsafeBitCast(value, to: UInt16.self))
+	}
+	
+	
+	func write(int24 value: Int32) throws {
+		try write(uint24: unsafeBitCast(value, to: UInt32.self))
+	}
+	
+	
+	func write(int32 value: Int32) throws {
+		try write(uint32: unsafeBitCast(value, to: UInt32.self))
+	}
+	
+	
+	func write(int64 value: Int64) throws {
+		try write(uint64: unsafeBitCast(value, to: UInt64.self))
+	}
+	
+}
+	
+
+
+	
+// MARK: - Types
+
+
+protocol BinaryStreamDestination {
+	var isEndOfStream: Bool { get }
+	var length: Int { get }
+	var position: Int { get }
+	func setPosition(_ pos: Int) throws
+	
+	func read(bytes: UnsafeMutableRawPointer, length: Int) throws
+	func readData(length: Int) throws -> Data
+}
+
+
+
+protocol MutableBinaryStreamDestination: BinaryStreamDestination {
+	//var isResizable: Bool { get }
+	func write(bytes: UnsafeRawPointer, length: Int) throws
+	func write(_ data: Data) throws
+}
+
+
+
+enum BinaryStreamError: Error {
+	/// Attempting to read or write data from outside the bounds of the data.
+	case outOfBounds
+	
+	/// The destination is not resizable
+	case notResizable
+}
+	
+	
+	
+
+
+
+
+// MARK: - Destinations
+extension BinaryStream {
+	
+	/// An NSData-backed destination which cannot be resized or written to
+	class MemoryDestination: BinaryStreamDestination {
+		
+		fileprivate var _data: NSData
+		
+		
+		init(data: NSData) {
+			_data = data
+		}
+		
+		
+		/// Returns an immutable copy of the data.
+		var data: NSData {
+			return _data.copy() as! NSData
+		}
+		
+		
+		/// Returns whether the current position is at the end of the stream
+		var isEndOfStream: Bool {
+			return position == length
+		}
+		
+		/// The length of the data in the stream.
+		var length: Int {
+			get {
+				return _data.length
+			}
+		}
+		
+		
+		/// The current position of the data in the stream. Reading and writing moves the position.
+		fileprivate(set) var position: Int = 0
+		
+		
+		/// Setting the position past the data length will throw 
+		func setPosition(_ pos: Int) throws {
+			if pos > length {
+				throw BinaryStreamError.outOfBounds
+			}
+			position = pos
+		}
+		
+		
+		func read(bytes: UnsafeMutableRawPointer, length: Int) throws {
+			guard _data.length >= position + length else {
+				throw BinaryStreamError.outOfBounds
+			}
+			_data.getBytes(bytes, range: NSMakeRange(Int(position), Int(length)))
+			position += length
+		}
+		
+		
+		func readData(length: Int) throws -> Data {
+			guard Int(_data.length) >= position + length else {
+				throw BinaryStreamError.outOfBounds
+			}
+			let data = _data.subdata(with: NSMakeRange(Int(position), Int(length)))
+			position += length
+			return data
+		}
+	}
+	
+	
+	
+	
+	
+	/// An NSData-backed destination which can be written to, an *may* be resizable if initialized to be so.
+	class MutableMemoryDestination: MemoryDestination, MutableBinaryStreamDestination {
+		
+		private let resizable: Bool
+		
+		
+		/// Initializes a writable, but not resizable, destination
+		override init(data: NSData) {
+			resizable = false
+			super.init(data: data)
+		}
+		
+		
+		/// Initializes a writable and resizable, destination
+		init(data: NSMutableData, resizable: Bool) {
+			self.resizable = resizable
+			super.init(data: data)
+		}
+		
+		
+		override var data: NSData {
+			return _data
+		}
+		
+		
+		/// Returns the underlying mutable data of the destination.
+		/// Be sure to correctly update the position of the destination
+		/// if you modify the length of the mutable data to be shorter
+		/// than the current position's value.
+		var mutableData: NSMutableData {
+			return _data as! NSMutableData
+		}
+		
+		
+		/// The length of the data in the stream.
+		func setLength(_ length: Int) throws {
+			guard resizable else {
+				throw BinaryStreamError.notResizable
+			}
+			
+			mutableData.length = Int(length)
+		}
+		
+		
+		/// Setting the position past the current length will grow the length of the stream. 
+		override func setPosition(_ pos: Int) throws {
+			if pos > length {
+				guard resizable else {
+					throw BinaryStreamError.notResizable
+				}
+				mutableData.increaseLength(by: Int(pos - length))
+			}
+			position = pos
+		}
+		
+		
+		func write(_ data: Data) {
+			data.withUnsafeBytes { (ptr: UnsafePointer) in
+				write(bytes: ptr, length: data.count)
+			}
+		}
+		
+		
+		func write(bytes: UnsafeRawPointer, length: Int) {
+			let mdata = mutableData
+			
+			if mdata.length >= position + length {
+				memcpy(mdata.mutableBytes, bytes, Int(length))
+			} else {
+				mdata.append(bytes, length: Int(length))
+			}
+			
+			position += length
+		}
+	}
+}
+
+
+
+
+
+
+// MARK: - Stream Extensions
+
+extension BinaryStream {
+	func readUTF8String() throws -> String {
+		let byteCount = Int(try readUInt32())
+		if byteCount == 0 {
+			return ""
+		}
+		
+		let ptr = UnsafeMutableRawBufferPointer.allocate(count: Int(byteCount))
+		try readBytes(ptr.baseAddress!, length: byteCount)
+		
+		guard let s = NSString(bytes: ptr.baseAddress!, length: Int(byteCount), encoding: String.Encoding.utf8.rawValue) else {
+			return ""
+		}
+		
+		return s as String
+	}
+}
+
+
+extension MutableBinaryStream {
+	func writeUTF8String(_ string: String) throws {
+		try string.utf8CString.withUnsafeBufferPointer({ (ptr: UnsafeBufferPointer<CChar>) in
+			precondition(ptr.count > 0, "Null-terminated utf8 buffer unexpectedly has count of 0")
+			let byteCount = ptr.count - 1 // Discount null character
+			try write(uint32: UInt32(byteCount))
+			if byteCount > 0 {
+				try write(bytes: ptr.baseAddress!, length: byteCount)
+			}
+		})
+	}
+}
+
